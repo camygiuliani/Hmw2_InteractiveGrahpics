d835ee8cf425d57683e4da22fe303846e8a048a0
Camilla <camilla.giuliani@live.com>
2025-03-26 17:25:22 +0100

    Implementing functions

--- project2.js
+++ project2.js
@@ -3,7 +3,14 @@
 // The given rotation value is in degrees.
 function GetTransform( positionX, positionY, rotation, scale )
 {
-	return Array( 1, 0, 0, 0, 1, 0, 0, 0, 1 );
+	const s=scale;
+	//conversion from degrees to radiants
+	const theta=rotation*(Math.PI/180);
+	const cost=Math.cos(theta);
+	const sint=Math.sin(theta);
+	const x=positionX;
+	const y=positionY;
+	return Array( s*cost,s*sint,0,-s*sint,s*cost,0,x,y,1 );
 }
 
 // Returns a 3x3 transformation matrix as an array of 9 values in column-major order.
@@ -11,5 +18,16 @@ function GetTransform( positionX, positionY, rotation, scale )
 // The returned transformation first applies trans1 and then trans2.
 function ApplyTransform( trans1, trans2 )
 {
-	return Array( 1, 0, 0, 0, 1, 0, 0, 0, 1 );
+	let result = new Array(9);
+
+    for (let row = 0; row < 3; row++) {
+        for (let col = 0; col < 3; col++) {
+            result[row + col * 3] =
+                trans2[row + 0 * 3] * trans1[0 + col * 3] +
+                trans2[row + 1 * 3] * trans1[1 + col * 3] +
+                trans2[row + 2 * 3] * trans1[2 + col * 3];
+        }
+    }
+
+    return result;
 }
